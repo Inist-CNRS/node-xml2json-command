dcfabeeffd19ef12b353801bc3076d3dd9995bb0
François Parmentier <francois.parmentier@gmail.com>
2012-11-15 16:30:56 +0100

    Main program.

---
+++ xml2json.js
@@ -0,0 +1,15 @@
+var xm = require('xml-mapping');
+var util = require('util');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+var xml = '';
+process.stdin.on('data', function (chunk) {
+  xml = xml + chunk;
+});
+
+process.stdin.on('end', function() {
+  var json = xm.load(xml);
+  console.log(JSON.stringify(json));
+});
