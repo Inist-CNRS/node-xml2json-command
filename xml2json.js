87d15e41c020e5432ec7ba4b4e175c2c22c30f51
kflo-m <kjetil@flovild-midtlie.name>
2013-12-06 09:26:37 +0100

    help option added

--- xml2json.js
+++ xml2json.js
@@ -1,7 +1,19 @@
 /*jshint node:true */
 "use strict";
 
-var xm = require('xml-mapping');
+var xm = require('xml-mapping'),
+    optimist = require('optimist'),
+    argv = optimist
+    .options('help',{alias:'h', describe: 'Show help'})
+    .options('caproot',{alias:'c',describe:'no root elem in JSON'})
+    .usage('\nRead standard input.\nUsage : $0 [--help] [--caproot]')
+    .argv; 
+
+
+if ( argv.help ){
+  optimist.showHelp();
+  process.exit(0);
+}
 
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
@@ -15,3 +27,5 @@ process.stdin.on('end', function() {
   var json = xm.load(xml);
   console.log(JSON.stringify(json, null, '\t'));
 });
+
+
